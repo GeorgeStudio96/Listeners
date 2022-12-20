e56f813c0b878acfb865a72fa0ef8c663716424e
George Ershov <geo.erh96@gmail.com>
2022-12-20 13:10:39 +0300

    ДЗ урока 11

--- script.js
+++ script.js
@@ -1,3 +1,81 @@
+const mainBtn = document.getElementById('btn')
+const input = document.getElementById('text')
+const square = document.querySelector('#square')
+const circle = document.querySelector('#circle')
+const btn = document.querySelector('#e_btn')
+const inputRange = document.querySelector('input[type=range]')
+const span = document.querySelector('#range-span')
+
+
+
+
+
+function inputValue(event) {
+    console.log(event.target.value);
+}
+
+input.addEventListener('input', inputValue)
+input.addEventListener('change', inputValue)
+
+
+
+
+const changeColor = function (e) {
+    e.stopPropagation();
+
+    if (input.value == 'black') {
+        square.style.backgroundColor = 'black'
+    } else if (input.value == 'blue') {
+        square.style.backgroundColor = 'blue'
+    } else {
+        square.style.backgroundColor = 'orange'
+    }
+
+    btn.style.display = 'none'
+
+
+}
+
+
+mainBtn.addEventListener('click', changeColor)
+
+
+
+
+const changeRange = function (event) {
+    console.dir(event.target);
+    span.innerHTML = event.target.value + '%'
+    circle.style.width = event.target.value + '%'
+    circle.style.height = event.target.value + '%'
+
+}
+
+
+inputRange.addEventListener('input', changeRange)
+inputRange.addEventListener('change', changeRange)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const btn = document.querySelector('#e_btn')
 const circle = document.querySelector('#circle')
 const square = document.querySelector('#square')
@@ -12,3 +90,5 @@ const toggler = function (event) {
 btn.addEventListener('click', toggler, true)
 circle.addEventListener('click', toggler, true)
 square.addEventListener('click', toggler, true)
+
+*/
